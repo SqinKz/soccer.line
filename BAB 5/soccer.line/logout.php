d6f74a76b7855746e0f1241f223a056a67b8fc50
SqinKz <adityaasp2@gmail.com>
2024-12-13 20:48:48 +0700

    Delete BAB 5 directory

--- BAB 5/soccer.line/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset(); // Menghapus semua session
-session_destroy(); // Menghancurkan session
-header("Location: login.php"); // Redirect ke halaman login
-exit();
-?>
